4f8bde567c2a75688a8b789e69219691bd400a31
Nayak <harshitnayak998@gmail.com>
2021-02-04 18:23:34 +0530

    decimal to binary

---
+++ java-programs/decimal_to_binary.java
@@ -0,0 +1,34 @@
+// this program converts a decimal number to a binary number.
+// Author@NAYAK
+// First of all we divide the decimal number with 2 
+// and store the value in an array
+// then we display the array..  
+import java.util.Scanner;
+
+public class decimal_to_binary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the decimal number:");
+        int dec = sc.nextInt();
+
+        int i = 0;
+        int bin[] = new int[100];
+
+        while(dec>0){
+            bin[i++] = dec % 2;// storing the remainder in the array
+            dec = Math.floorDiv(dec , 2);// getting the quotient in floor value
+            
+        }
+
+        System.out.println("The binary conversion is:");
+        // printing the array in reverse...
+        // length of array to be printed depends on the number of divisions
+        for(int j = i-1; j>=0; j--){
+            System.out.print(bin[j]);
+        }
+        
+
+        sc.close();
+    }
+    
+}
