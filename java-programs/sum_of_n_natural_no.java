cf3dc87afd02261475a7ccdffc63d3a58f77a692
Nayak <harshitnayak998@gmail.com>
2021-02-07 17:02:40 +0530

    IntelliJ IDEA made some messs , just cleaned it

--- java-programs/sum_of_n_natural_no.java
+++ java-programs/sum_of_n_natural_no.java
@@ -9,9 +9,10 @@ public class sum_of_n_natural_no {
         }
     }
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
         System.out.println("TO FIND THE SUM OF N NATURAL NUMBERS:- \n enter n:");
-        System.out.println("The sum is" +sumn(x));
+        int x = scan.nextInt();
+        System.out.println("The sum is " +sumn(x));
     }
 }
