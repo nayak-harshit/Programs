44b12b025126e80b4ff498845a423c826d44df20
Nayak <harshitnayak998@gmail.com>
2021-02-25 20:04:20 +0530

    arm

--- java-programs/armstrong_number.java
+++ java-programs/armstrong_number.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class armstrong_number {
-    static boolean arm_or_not(int d){
+    public static boolean armstrong_or_not(int d){
        
-        int r = 0;
-        int q = d;
- 
-        while (q >0) {
+        int sum_of_cubes = 0;
+
+        while (d >0) {
             // when we divide by 10 the remainder is the last digit of the number..
-            r = r + ((q % 10)*(q % 10)*(q % 10));
-            q = Math.floorDiv(q, 10);
+            sum_of_cubes = sum_of_cubes + ((d % 10)*(d % 10)*(d % 10));
+            // after it we change the number to the quotient of the division with 10, so that the next time we get previous digit .
+            d = Math.floorDiv(d, 10);
         }
-        if(r==d) return true;
-        return false;
+        if(sum_of_cubes == d) return  true;
+        return  false;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to check your number is ARMSTRONG or not: ");
         int a = sc.nextInt();
-        System.out.println(arm_or_not(a));
+        System.out.println(armstrong_or_not(a));
         sc.close();
     }
     
