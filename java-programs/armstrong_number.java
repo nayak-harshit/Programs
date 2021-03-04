ab05b0f7966cd8d5b2d2766683131d0131d8dddb
Nayak <harshitnayak998@gmail.com>
2021-03-04 08:30:36 +0530

    changed armstrong.java for gfg

--- java-programs/armstrong_number.java
+++ java-programs/armstrong_number.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class armstrong_number {
-    public static boolean armstrong_or_not(int d){
+    static boolean armstrong_or_not(int d){
        
         int sum_of_cubes = 0;
+        int q;
+        q = d;
 
-        while (d >0) {
+        while (q >0) {
             // when we divide by 10 the remainder is the last digit of the number..
-            sum_of_cubes = sum_of_cubes + ((d % 10)*(d % 10)*(d % 10));
+            sum_of_cubes = sum_of_cubes + ((q % 10)*(q % 10)*(q % 10));
             // after it we change the number to the quotient of the division with 10, so that the next time we get previous digit .
-            d = Math.floorDiv(d, 10);
+            q = Math.floorDiv(q, 10);
         }
-        if(sum_of_cubes == d) return  true;
-        return  false;
+        return sum_of_cubes == d;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to check your number is ARMSTRONG or not: ");
