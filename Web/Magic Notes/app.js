4c4b380f0b9cfbd69ddf30173c5ec49235ba6df4
Nayak <harshitnayak998@gmail.com>
2021-06-09 15:25:35 +0530

    changed magic notes

--- Web/Magic Notes/app.js
+++ Web/Magic Notes/app.js
@@ -3,8 +3,10 @@ showNotes();
 let addNote = document.getElementById('addBtn');
 let addTxt = document.getElementById('addTxt');
 // console.log(addBtn)
+
+
 addNote.addEventListener('click', function (e) {
-    console.log('this is event listener for the button')
+    // console.log('this is event listener for the button')
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -17,7 +19,9 @@ addNote.addEventListener('click', function (e) {
     addTxt.value = "";
     console.log(notesObj);
     showNotes();
-})
+});
+
+
 function showNotes() {
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -28,13 +32,12 @@ function showNotes() {
     let html = "";
     notesObj.forEach(function (element, index) {
         html += `
-              <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                      <div class="card-body">
-                          <h5 class="card-title">Note ${index + 1}</h5>
-                          <p class="card-text"> ${element}</p>
-                          <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                      </div>
-                  </div>`;
+        <div class="noteCard my-2 mx-2 card" >
+        <div class="card-body">
+            <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text"> ${element}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button> <button value="${index}" onclick="editNote(this.value)" class="btn btn-primary">Edit Note</button>
+        </div>`;
     });
     let notesElem = document.getElementById('notes');
     if (notesObj.length != 0) {
@@ -42,8 +45,9 @@ function showNotes() {
     } else {
         notesElem.innerHTML = `Your Notes will appear here... `
     }
-
 }
+
+
 function deleteNote(index) {
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -57,22 +61,40 @@ function deleteNote(index) {
     showNotes();
 
 }
+
+
 let search = document.getElementById('searchTxt');
+
 search.addEventListener('input', function () {
-    console.log('input event');let inputVal = search.value.toLowerCase();
+    console.log('input event'); let inputVal = search.value.toLowerCase();
     // console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
+    Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
-        if(cardTxt.includes(inputVal)){
+        if (cardTxt.includes(inputVal)) {
             element.style.display = "block";
         }
-        else{
+        else {
             element.style.display = "none";
         }
         // console.log(cardTxt);
     })
-})
+});
+
+
+function editNote(value) {
+    // console.log('edit text');
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    // console.log(value);
+    // console.log(notesObj[value]);
+    addTxt.value = notesObj[value];
+    deleteNote(value);
+}
 
 
 
