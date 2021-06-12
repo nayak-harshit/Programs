440aa1254a84b33a5c5876eac156c4ebd18efc5f
Nayak <harshitnayak998@gmail.com>
2021-06-12 20:31:05 +0530

    enter title added

--- Web/Magic Notes/app.js
+++ Web/Magic Notes/app.js
@@ -2,12 +2,13 @@ console.log('Hello this is app.js');
 showNotes();
 let addNote = document.getElementById('addBtn');
 let addTxt = document.getElementById('addTxt');
+let addTitle = document.getElementById('addTitle');
 // console.log(addBtn)
 
 
 addNote.addEventListener('click', function (e) {
     // console.log('this is event listener for the button')
-    let addTxt = document.getElementById("addTxt");
+    // let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -22,29 +23,50 @@ addNote.addEventListener('click', function (e) {
         titleObj = JSON.parse(titles);
     }
 
+    titleObj.push(addTitle.value);
+    localStorage.setItem('titles', JSON.stringify(titleObj));
+
     notesObj.push(addTxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj));
+
+    addTitle.value = "";
     addTxt.value = "";
+    console.log(titleObj);
     // console.log(notesObj);
     showNotes();
 });
 
 
 function showNotes() {
+    //local storage of titles
+    let titles = localStorage.getItem("titles");
+    if (titles == null) {
+        titleObj = [];
+    } else {
+        titleObj = JSON.parse(titles);
+    }
+
+    // titleObj.forEach(function (element, index )){
+    //     if (element == undefined) {
+    //         titleObj[index] = ''
+    //     }
+    // }
+
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
+    
     let html = "";
     notesObj.forEach(function (element, index) {
         html += `
         <div class="noteCard my-2 mx-2 card"style="width: 20rem; >
         <div class="card-body">
-            <h5 class="card-title" id="title${index}">Note ${index + 1}</h5>
+            <h5 class="card-title" id="title${index}">${titleObj[index]}</h5>
             <p class="card-text"> ${element}</p>
-            <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary my-1">Delete Note</button> <button value="${index}" onclick="editNote(this.value)" class="btn btn-primary">Edit Note</button>
+            <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary my-1">Delete Note</button> <button value="${index}" onclick="editNote(this.value)" class="btn btn-primary my-1">Edit Note</button>
         </div>
         </div>
         `;
@@ -65,6 +87,17 @@ function deleteNote(index) {
     } else {
         notesObj = JSON.parse(notes);
     }
+
+    let titles = localStorage.getItem("titles");
+    if (titles == null) {
+        titleObj = [];
+    } else {
+        titleObj = JSON.parse(titles);
+    }
+
+
+    titleObj.splice(index, 1);
+    localStorage.setItem("titles", JSON.stringify(titleObj));
 
     notesObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
@@ -100,11 +133,50 @@ function editNote(value) {
     } else {
         notesObj = JSON.parse(notes);
     }
+    let titles = localStorage.getItem("titles");
+    if (titles == null) {
+        titleObj = [];
+    } else {
+        titleObj = JSON.parse(titles);
+    }
+
+
     // console.log(value);
     // console.log(notesObj[value]);
+    addTitle.value = titleObj[value];
     addTxt.value = notesObj[value];
     deleteNote(value);
 }
 
+
+const preBook = document.getElementById('preBookmark');
+const postBook = document.getElementById('postBookmark');
+const preFav = document.getElementById('preFav');
+const postFav = document.getElementById('postFav');
+
+// console.log(preBook);
+// console.log(postBook);
+// console.log(preFav);
+// console.log(postFav);
+
+preBook.addEventListener('click', function(){
+    preBook.classList.add('d-none');
+    postBook.classList.remove('d-none');
+});
+
+postBook.addEventListener('click', function(){
+    postBook.classList.add('d-none');
+    preBook.classList.remove('d-none');
+});
+
+preFav.addEventListener('click', function(){
+    preFav.classList.add('d-none');
+    postFav.classList.remove('d-none');
+});
+
+postFav.addEventListener('click', function(){
+    postFav.classList.add('d-none');
+    preFav.classList.remove('d-none');
+});
 
 
