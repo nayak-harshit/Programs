705b51023e74bc129eed201bc40a95c7b6e3dc2e
Nayak <harshitnayak998@gmail.com>
2022-01-19 09:57:38 +0530

    chess clock only green color changes now

--- Android_Studio/ChessClock/app/src/main/java/com/example/chessclock/MainActivity.java
+++ Android_Studio/ChessClock/app/src/main/java/com/example/chessclock/MainActivity.java
@@ -27,47 +27,44 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("TAG", "playOne: ");
 
-
-        if(startOne.getTag().toString() == "0"){
-            //Green
-
+        Log.d("TAG", "playOne: "+ startOne.getTag().toString());
+        if(startOne.getTag().toString().equals("1")){
+            //Green - timer is running
+            Log.d("TAG", "playOne: green");
             startOne.setBackground(this.getDrawable(R.color.red));
             startTwo.setBackground(this.getDrawable(R.color.green));
-            startOne.setTag("1");
-            startTwo.setTag("0");
-            Log.d("TAG", "playOne: green");
+            startOne.setTag("0");
+            startTwo.setTag("1");
+
 
 
         }else{
-            //Red
-
-            startOne.setBackground(this.getDrawable(R.color.green));
-            startTwo.setBackground(this.getDrawable(R.color.red));
-            startOne.setTag("0");
-            startTwo.setTag("1");
-            Log.d("TAG", "playOne: red");
+            //Red - timer is stopped
+            Log.d("TAG", "playOne: else");
+          return;
 
         }
 
     }
 
     public void playTwo(View view) {
-        if(startTwo.getTag().toString() == "0"){
+        Log.d("TAG", "playTwo: ");
+        if(startTwo.getTag().toString().equals("1")){
             //Green
 
             startOne.setBackground(this.getDrawable(R.color.green));
             startTwo.setBackground(this.getDrawable(R.color.red));
-            startOne.setTag("0");
-            startTwo.setTag("1");
-            Log.d("TAG", "playOne: green");
-        }else{
-            //Red
-
-            startOne.setBackground(this.getDrawable(R.color.red));
-            startTwo.setBackground(this.getDrawable(R.color.green));
             startOne.setTag("1");
             startTwo.setTag("0");
-            Log.d("TAG", "playOne: red");
+            Log.d("TAG", "playTwo: green");
+        }else{
+            //Red
+            return;
+//            startOne.setBackground(this.getDrawable(R.color.red));
+//            startTwo.setBackground(this.getDrawable(R.color.green));
+//            startOne.setTag("1");
+//            startTwo.setTag("0");
+//            Log.d("TAG", "playOne: red");
 
         }
     }
