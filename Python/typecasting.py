0783d75438aa4a9f6962722447a0c9b73d058e9a
Nayak <harshitnayak998@gmail.com>
2022-05-23 14:51:31 +0530

    fibonacci in python

--- Python/typecasting.py
+++ Python/typecasting.py
@@ -4,15 +4,15 @@
 var = 34
 var2 = 3443.24
 # the below both variables are string variables...
-var3 = 'var3'
+var3 = "var3"
 var4 = "var4"
 print(type(var))
 print(type(var2))
 print(type(var3))
 print(type(var4))
 
-strr = '23'
-str2 = '687'
+strr = "23"
+str2 = "687"
 # this is typecasting a string into integer using int()function..
 print(int(strr) + int(str2))
 print(type(float(var)))  # --> converts int to float
@@ -20,7 +20,5 @@ print(str(var))
 # print(int(var3)) --> produces error
 
 # to print same thing many times..
-print(10*var3)  # --> this prints var3 10 times
-print(10*var)   # --> but this prints 34*10 i.e. 340
- 
-
+print(10 * var3)  # --> this prints var3 10 times
+print(10 * var)  # --> but this prints 34*10 i.e. 340
